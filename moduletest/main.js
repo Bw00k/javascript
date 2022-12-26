1d71e502c48b74500d1c042ec5cab4eae7bff8fc
bw00k <bw00k@naver.com>
2022-12-26 17:31:38 +0900

    26

---
+++ moduletest/main.js
@@ -0,0 +1,10 @@
+/* main.js*/
+//import * as md from "./module.js";
+import { module as mod, area } from "./module.js";
+import sq, { circle } from "./moduleone.js";
+//md.module("test");
+//md.area(10, 20);
+mod("test");
+area(10, 20);
+sq();
+circle();
---
+++ moduletest/module.js
@@ -0,0 +1,9 @@
+/*module.js*/
+function module(msg) {
+  console.log("msg:" + msg);
+}
+function area(a, b) {
+  console.log("area:" + a * b);
+}
+
+export { module, area };
